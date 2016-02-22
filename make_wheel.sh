36218928ee045706730678f6b965e9cd40dc33ff
Artem Tarasov <artem.tarasov@embl.de>
2016-02-22 09:01:41 -0800

    use CMAKE_C_COMPILER/CMAKE_CXX_COMPILER explicitly
    relying on CC/CXX env. vars leads to dummy.c being compiled with
    Clang-specific flags on Mac OS X, leading to a build failure.

--- make_wheel.sh
+++ make_wheel.sh
@@ -1,6 +1,6 @@
 #/usr/bin/env bash
 
 cd ims-cpp && mkdir -p build && cd build &&\
-    cmake -DCMAKE_BUILD_TYPE=Release .. &&\
+    cmake -DCMAKE_C_COMPILER=gcc-5 -DCMAKE_CXX_COMPILER=g++-5 -DCMAKE_BUILD_TYPE=Release .. &&\
     make ms_cffi && cd ../../ &&\
     python setup.py bdist_wheel
