c2e5456d4dee0795473a68d47c99d76d60f1ba83
Artem Tarasov <artem.tarasov@embl.de>
2016-02-23 16:44:05 +0100

    added script for making python eggs on linux
    shared lib is built in a container with old glibc;
    since _binary_ wheels on Linux are unsupported (exactly because
    not everyone is even aware of the necessity to use old systems for
    builds, so a file naming scheme is required that would include
    at least glibc version), I call 'bdist_egg' command instead.

---
+++ make_linux_egg.sh
@@ -0,0 +1,10 @@
+#/usr/bin/env bash
+
+mnt="/code"
+build_cmd="cd $mnt/build && cmake -DCMAKE_BUILD_TYPE=Release .. && make ms_cffi"
+image=devtoolset-3
+
+cd ims-cpp && docker build -t $image . && mkdir -p build &&\
+    echo $build_cmd &&\
+    docker run -v `pwd`:$mnt $image /bin/bash -c "$build_cmd" &&\
+    cd .. && python setup.py bdist_egg
