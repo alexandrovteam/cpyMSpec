4e0e3da6d80eca187380f5a6a6fd096c0b0ecd56
Artem Tarasov <artem.tarasov@embl.de>
2016-02-29 13:45:32 +0100

    fix exception handling on OS X

--- make_osx_wheel.sh
+++ make_osx_wheel.sh
@@ -3,7 +3,9 @@
 # the official distribution from python.org should be used instead of the system one,
 # see reasons here: https://github.com/MacPython/wiki/wiki/Spinning-wheels
 
+rm -rf dist &&\
 cd ims-cpp && mkdir -p build && cd build &&\
     cmake -DCMAKE_C_COMPILER=gcc-5 -DCMAKE_CXX_COMPILER=g++-5 -DCMAKE_BUILD_TYPE=Release .. &&\
     make ms_cffi && cd ../../ &&\
-    /usr/local/bin/python setup.py bdist_wheel
+    /usr/local/bin/python setup.py bdist_wheel &&\
+    delocate-wheel dist/cpyMSpec*.whl
