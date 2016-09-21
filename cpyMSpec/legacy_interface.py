0194e8790d60c81df12e3a1b7a82127d7a4bd2bf
Artem Tarasov <artem.tarasov@embl.de>
2016-09-21 18:49:36 +0200

    fix complete_isodist

--- cpyMSpec/legacy_interface.py
+++ cpyMSpec/legacy_interface.py
@@ -1,4 +1,4 @@
-from cpyMSpec import isotopePattern, centroidize
+from cpyMSpec import isotopePattern, InstrumentModel, ProfileSpectrum
 
 # layer of compatibility with the original pyMSpec.pyisocalc module
 
@@ -14,22 +14,23 @@ def complete_isodist(sf, sigma=0.001, cutoff_perc=0.1, charge=None, pts_per_mz=1
     cutoff = cutoff_perc / 100.0
     fwhm = sigma * 2.3548200450309493  # the exact ratio is 2 \sqrt{2 \log 2}
     abs_charge = max(1, abs(charge))
-    p = isotopePattern(str(sf), cutoff / 10.0).charged(charge)
+    p = isotopePattern(str(sf), cutoff / 10.0)
+    p.addCharge(charge)
 
     mz = min(p.masses) / abs_charge
     resolution = mz / fwhm
 
     mzs = np.arange(min(p.masses) / abs_charge - 1,
                     max(p.masses) / abs_charge + 1, 1.0/pts_per_mz)
-    intensities = np.asarray(p.envelope(resolution)(mzs * abs_charge))
+    instr = InstrumentModel('tof', resolution)
+    intensities = np.asarray(p.envelope(instr)(mzs * abs_charge))
     intensities *= 100.0 / intensities.max()
 
     ms = MassSpectrum()
     ms.add_spectrum(mzs, intensities)
 
-    p = centroidize(mzs, intensities, 5)
-    p *= 100
-    p.removeIntensitiesBelow(cutoff_perc)
+    p = ProfileSpectrum(mzs, intensities).centroids(5)
+    p.removeIntensitiesBelow(cutoff)
     p.sortByMass()
-    ms.add_centroids(p.masses, p.intensities)
+    ms.add_centroids(p.masses, np.array(p.intensities) * 100)
     return ms
